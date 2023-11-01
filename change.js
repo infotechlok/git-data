7e03b1f43a5dd5a766eb03038a21e417ee8769a9
Lokesh Sharma <lk303897@gmail.com>
2023-11-01 15:18:34 +0530

    version2

--- change.js
+++ change.js
@@ -1,3 +1,3 @@
-console.log('changed daa');
-console.log('git learning');
+console.log('changed data for version2');
+console.log('git learning for about the git versions');
 console.log('git files are made by me');
